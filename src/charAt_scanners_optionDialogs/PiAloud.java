1dbc5688c9e7548dde613fe8036a474ce2a6f070
league <league@imac-4.attlocal.net>
2019-01-26 12:58:35 -0800

    finish only one of the eclipse.

--- src/charAt_scanners_optionDialogs/PiAloud.java
+++ src/charAt_scanners_optionDialogs/PiAloud.java
@@ -2,27 +2,33 @@ package charAt_scanners_optionDialogs;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
 import java.io.IOException;
 import java.util.Scanner;
 
 public class PiAloud {
 
 	// 1. Make a main method and make sure your program can run
+public static void main(String[] args) {
+	
 
 	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
-
+String pi ="3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862";
 	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
 
-	// 4. Print ALL the digits of of Pi (hint: use a loop)
+	System.out.println("pi.charAt(0)"+"pi.charAt(1)");
+	
 
+
+
+	// 4. Print ALL the digits of of Pi (hint: use a loop)
+for(int i = 0; i< i++);
 	// 5. Use the speak() method to speak all the digits of Pi.
 
 	// [ADVANCED]
 	// *6. Get a character from the user using the getInputFromUser() method
 	// *7. Compare the users' char to the next digit of Pi
 	// *8. If they are correct, print out "correct". If they are not, print "incorrect" to System.err.println
-
+}
 	static void speak(char characterToSpeak) {
 		try {
 			Runtime.getRuntime().exec("say " + characterToSpeak).waitFor();
--- src/charAt_scanners_optionDialogs/StringMethods1.java
+++ src/charAt_scanners_optionDialogs/StringMethods1.java
@@ -12,23 +12,34 @@ public class StringMethods1 {
 		
 		
 // 1. Create a String variable and initialize it to whatever you want
-String abc = JOptionPane.showMessageDialog(parentComponent, message, title, messageType, icon);
+		String abc="abcdefga";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
+		System.out.println(abc.charAt(3)); 
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
+		System.out.println(abc.length());
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
-		
+		System.out.println("--4--");
+		for(int i = 0; i<abc.length(); i++) {
+		    System.out.println(abc.charAt(i));
+		}
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
-		
+		System.out.println("--5--");
+		char ch='e';
+		for(int i = 0; i<abc.length(); i++) {
+		   if(ch==abc.charAt(i)) {
+			   System.out.println("Char equals "+abc.charAt(i)+" at index "+i);
+			   break;
+			   
+			   
+		   }
+		}
 	}
 }
 
